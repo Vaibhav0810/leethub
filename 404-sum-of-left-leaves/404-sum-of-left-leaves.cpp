3dfc3261d8a4a0b57f0f48385770c1fbd45177bb
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2022-08-07 00:24:41 +0530

    Time: 7 ms (52.66%), Space: 13.4 MB (81.31%) - LeetHub

--- 404-sum-of-left-leaves/404-sum-of-left-leaves.cpp
+++ 404-sum-of-left-leaves/404-sum-of-left-leaves.cpp
@@ -17,7 +17,8 @@ public:
             return 0;
         }
          
-        
+        // isme hm dekkh rhe h phle agr root ka koi left h?
+        // agr h to theek h vo check kro vo leaf h ya ni h to sum mei add ni to return
         if(root->left && root->left->right==NULL && !root->left->left)
             sum=sum+root->left->val;
         
