4f1e652a46c61003224f87b96b5cc8b130d85b29
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2022-08-09 18:22:26 +0530

    Time: 26 ms (46.04%), Space: 14.3 MB (57.51%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -7,13 +7,22 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
+/*
+    isme phle 2-3 condition dekhni h 
+    1. if root==p ya q ho jaata h to mtlb uske aage jaane ki jroort ni h ans vo hoga ya uss se phle
+    2. agr return krte hue root ke left right se null ni aa rha dono se to vo root hi ans h
+    3. agr kisi bhi ik jgh se null aa rha h to uska opposite return krdo
+    like agr left se null aarha h to right return aise
+*/
+
 class Solution {
 public:
   
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         
-        if(root==NULL) return NULL;
-        if(root==p || root==q) return root;
+    if(root==NULL) return NULL;
+    if(root==p || root==q) return root; 
         
          TreeNode* left=lowestCommonAncestor(root->left,p,q);
          TreeNode* right=lowestCommonAncestor(root->right,p,q);
