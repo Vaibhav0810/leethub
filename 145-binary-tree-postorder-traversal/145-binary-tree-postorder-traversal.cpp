999f6cb339ca98edfc55675538d3dfed6a9a267b
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2022-08-10 23:31:07 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (63.74%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
@@ -30,30 +30,64 @@ public:
     
     // USING 2 STACKS
     // Time-O(N) Space-O(2N)
-    stack<TreeNode*> st1;
-    stack<TreeNode*> st2;
-    vector<int> ans;
+//     stack<TreeNode*> st1;
+//     stack<TreeNode*> st2;
+//     vector<int> ans;
+//     vector<int>postorderTraversal(TreeNode* root){
+        
+//         if(root==NULL) return ans;
+        
+//         st1.push(root);
+//         while(!st1.empty()){
+//             st2.push(st1.top());
+//             TreeNode*temp=st1.top();
+//             st1.pop();
+//             if(temp->left!=NULL){
+//                 st1.push(temp->left);
+//             }
+//             if(temp->right!=NULL) st1.push(temp->right);
+//         }
+        
+//         while(!st2.empty()){
+//         ans.push_back(st2.top()->val); 
+//         st2.pop();
+//         }
+        
+//         return ans;
+        
+//     }
+    
+    // Using One stack(video link in notes)
+    
     vector<int>postorderTraversal(TreeNode* root){
-        
-        if(root==NULL) return ans;
-        
-        st1.push(root);
-        while(!st1.empty()){
-            st2.push(st1.top());
-            TreeNode*temp=st1.top();
-            st1.pop();
-            if(temp->left!=NULL){
-                st1.push(temp->left);
+        stack<TreeNode*>st;
+        vector<int>ans;
+        TreeNode* curr=root;
+        TreeNode* temp;
+        while(curr!=NULL ||!st.empty()){
+            if(curr!=NULL){
+                st.push(curr);
+                curr=curr->left;
             }
-            if(temp->right!=NULL) st1.push(temp->right);
+            else{
+                temp=st.top()->right;
+                if(temp==NULL){
+                    temp=st.top();
+                    ans.push_back(st.top()->val);
+                    st.pop();
+                    while(!st.empty() && temp==st.top()->right){
+                        temp=st.top();
+                        st.pop();
+                        ans.push_back(temp->val);
+                    }
+                }
+                else{
+                    curr=temp;
+                }
+                    
+            }
         }
-        
-        while(!st2.empty()){
-        ans.push_back(st2.top()->val); 
-        st2.pop();
-        }
-        
         return ans;
-        
     }
+    
 };
