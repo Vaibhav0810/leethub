d96798975a577c61af5bed609fe6019cb7754771
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2022-08-10 15:40:27 +0530

    Time: 31 ms (10.34%), Space: 21.6 MB (88.87%) - LeetHub

---
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    // iss vale solution mei [2147483647] ye test case pass ni hoga bcoz INT_MAX ki value itni ni hoti
+    // same for minimum
+    // long pass krenege to chl jayega
+     bool help(TreeNode* root,long max,long min){
+         if(root==NULL) return 1;
+             
+             if(root->val>min && root->val<max){
+                 return help(root->right,max,root->val) && help(root->left,root->val,min);
+             }
+         else return false;
+             
+     }
+    
+    bool isValidBST(TreeNode* root) {
+        
+//         if(root==NULL) return 1;
+//         if(root->left==NULL && root->right==NULL) return 1;
+      
+        
+//         if(root->left==NULL&& root->right!=NULL){
+//             if(root->right->val>!root->val){
+//                 return false;
+//             }
+//         }
+//         else if(root->right==NULL&& root->left!=NULL){
+//             if(root->left->val<!root->val) return 0;
+//         }
+//         else if(root->right!=NULL && root->left!=NULL){
+//         if(root->left->val<!root->val && root->right->val>!root->val){
+//             return 0;
+//         }
+//         }
+//         //else{return false;}
+        
+//         return isValidBST(root->left) && isValidBST(root->right);
+        
+        return help(root,LONG_MAX,LONG_MIN);
+        
+    }
+};
