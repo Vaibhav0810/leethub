0a5e90719f8c55b4f0cf5eab076968f8857bd7f9
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2022-08-12 22:22:53 +0530

    Time: 3 ms (85.34%), Space: 12.1 MB (20.80%) - LeetHub

---
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+/* 
+    use level order traversal and maintain a secondary vector named v and after loop end push the final element of v
+    in ans vector
+*/
+
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int>ans;
+        if(root==NULL) return ans;
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty()){
+            
+            int n=q.size();
+            
+            vector<int>v;
+            
+            for(int i=0;i<n;i++){
+                TreeNode* temp=q.front();
+                q.pop();
+                if(temp->left) q.push(temp->left);
+                if(temp->right) q.push(temp->right);
+                v.push_back(temp->val);
+            }
+            int x=v.size()-1;       // storing last element
+            ans.push_back(v[x]);    // pushing last element of v in ans
+        }
+        return ans;
+    }
+};
