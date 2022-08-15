35bf1be583e61094b6b956b334fe98e52fc1031d
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2022-08-16 00:42:05 +0530

    Time: 66 ms (44.35%), Space: 32.4 MB (61.87%) - LeetHub

--- 617-merge-two-binary-trees/617-merge-two-binary-trees.cpp
+++ 617-merge-two-binary-trees/617-merge-two-binary-trees.cpp
@@ -11,47 +11,28 @@
  */
 class Solution {
 public:
-    
-//     TreeNode* help(TreeNode* root1,TreeNode* root2){
-//         TreeNode* merged=new TreeNode();
-//         if(root1!=NULL && root2!=NULL){
-//             merged->val=root1->val+root2->val;
-          
-//         }
+   // Time and space=O(N)
+    // isme ye ho rha h phle  agr dono root null h to null return krdo
+    // agr ik null h to opposite return krdo
+    // now root1 mei root1+root2 daalenghe
+    // but jb root1 null hoga ye uddhr ni jaayega agr aise krenge to
+    //mergeTrees(root1->left,root2->left);(sirf aise krenge to ni hoga)
+    //bcoz vo null h udhr jaakr retuyrn kr dega
+    // isilie hm root1 ke left ya right ko uske eq krenge jiss se agr udhr null bhi ho to kuch bnn jaaye udhr
+    // 
+    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
+        if(root1==NULL && root2==NULL) return NULL;
+         if(root1==NULL) {
+             
+             return root2;
+         }
+         if(root2==NULL) {
+             return root1;
+         }
         
-//         else if(root1==NULL && root2!=NULL)
-//             merged->val=root2->val;
-//         else if(root2==NULL && root1!=NULL){
-//             merged->val=root1->val;
-//         }
-        
-//         help(root1->left,root2->left)&&help(root1->right,root2->right); 
-    
-        
-//         return merged;
-//     }
-    
-//     TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
-        
-//         if(root1==NULL && root2==NULL){
-//             return 0;
-//         }
-        
-//         return help(root1,root2);
-        
-//     }
-    
-    // https://leetcode.com/problems/merge-two-binary-trees/discuss/588123/~100.00-fast-in-run-time-and-memory-RecursiveIterativeBFSDFS
-    
-    TreeNode* mergeTrees(TreeNode* t1,TreeNode *t2){
-        if(t1 && t2){
-            TreeNode* root=new TreeNode(t1->val+t2->val);
-            root->left=mergeTrees(t1->left,t2->left);
-            root->right=mergeTrees(t1->right,t2->right);
-            return root;
-        }
-        else{
-            return t1?t1:t2;
-        }
+        root1->val=root1->val+root2->val;
+        root1->left=mergeTrees(root1->left,root2->left);
+        root1->right=mergeTrees(root1->right,root2->right);
+        return root1;
     }
 };
