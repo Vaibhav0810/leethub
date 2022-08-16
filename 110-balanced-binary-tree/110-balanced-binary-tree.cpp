6c8e01c14deaa1ee66f7ea5baab926c428d1d4c3
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2022-08-16 19:41:58 +0530

    Update 110-balanced-binary-tree.cpp

--- 110-balanced-binary-tree/110-balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/110-balanced-binary-tree.cpp
@@ -11,29 +11,46 @@
  */
 class Solution {
 public:
-    bool ans;
-    int help(TreeNode* root){
+    // brute time-O(N^2)
+  /* int height(TreeNode* root){
+        if(root==NULL) return 0;
         
-        if(!root) return 0; //iska mtlb h if root==NULL 
-        
-        if(ans==false) return 0; 
-        
-        int lH=help(root->left);
-        int rh=help(root->right);
-        
-        if(abs(lH-rh)>1){ // abs krna zroori hbcoz agr right bda hua to firr true dikha dega ye
-            ans=false;
-        }
-        
-        return 1+max(lH,rh);
-        
+        return max(height(root->left),height(root->right))+1;
     }
     
     bool isBalanced(TreeNode* root) {
+        
+        if(root==NULL) return 1;
+        //isBalanced(root->left);
+        int lh=height(root->left);
+        int rh=height(root->right);
+        
+        
+        if(abs(lh-rh)>1) return false;
+        else return isBalanced(root->left) && isBalanced(root->right);
+    }
+};
+*/  
+// optimal O(N) bottom up approach
+    bool ans;
+    int help(TreeNode* root){
+        if(root==NULL) return 0;
+        if(ans==false) return 0;
+        
+        int lh=help(root->left);
+        int rh=help(root->right);
+        
+        if(abs(lh-rh)>1) ans=false;
+        return 1+max(lh,rh);
+    }
+    
+    bool isBalanced(TreeNode* root){
+    
+        
         ans=true;
         help(root);
         return ans;
-        
+    
         
     }
 };
