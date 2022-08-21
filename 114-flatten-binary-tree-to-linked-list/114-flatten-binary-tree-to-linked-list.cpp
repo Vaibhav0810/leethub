c7ad5c14cdd9e8f2532a44fca75fdac1940564a3
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2022-08-21 20:14:47 +0530

    Time: 14 ms (29.60%), Space: 12.6 MB (78.35%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
@@ -34,7 +34,7 @@ public:
     // stack based (iterative)
     //time-O(N) space-O(N)
     
-    void flatten (TreeNode* root){
+/*    void flatten (TreeNode* root){
         stack<TreeNode*>st;
         if(root==NULL) return;
         st.push(root);
@@ -51,6 +51,36 @@ public:
                 temp->right=st.top();
             }
             temp->left=NULL;
+        }
+        
+    }
+    */
+    
+    // Morris Traversal 
+    // space-o(1) time-o(n)
+    
+    // in this we will make a thread b/w root left subttree ke right most element and root-> right pr 
+    
+    void flatten(TreeNode* root){ 
+        if(root==NULL)return ;
+        
+        TreeNode* temp=root;
+        
+        while(temp){
+            if(temp->left!=NULL){
+                
+                TreeNode* prv=temp->left;
+                
+                while(prv->right){
+                    prv=prv->right;
+                }
+                 prv->right=temp->right;
+                temp->right=temp->left;
+                temp->left=NULL;
+                
+               
+            }
+            temp=temp->right;
         }
         
     }
