aa1d63b46826847ec8ce1836c50045886ce66f0a
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2022-08-22 23:37:03 +0530

    Time: 39 ms (19.59%), Space: 24.2 MB (34.15%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
@@ -11,6 +11,16 @@
  */
 class Solution {
 public:
+    
+    //          BRUTE
+    // we can store all elements in an array and then sort the array and return kth element from first
+    
+    //  optimised
+    // first we go to the lowest element in the tree then we increase the count and if count becomes equal to k we return root->val 
+//else we go to root->left if it exist
+    // Time-o(n) space-0(N)
+ 
+    /*
 int count=0;
     int ans=0;
     int kthSmallest(TreeNode* root, int k) {
@@ -34,5 +44,57 @@ int count=0;
         return ans;
         
     }
+    */
+    //          iterative
+    // take a stack and push all left element in it the check ik --k==0 if yes then return st.top()else if st.top() left exist then push it into stack and keep on repeating 
+    
+    
+    
+    
+    //            MORRIS TRAVERSAL
+    // bcz inorder traversal lgg rhi h iss q mei
+    // TIME-O(N) space-O(1)
+    
+    
+    int kthSmallest(TreeNode* root,int k){
+        if(root==NULL) return 0;
+        int count=0;
+         int ans=0;
+        TreeNode* temp=root;
+        while(temp!=NULL){
+            if(temp->left==NULL){
+                count++;
+                if(count==k){
+                    ans=temp->val;
+                    
+                  //  break; why is it giving eroor
+                }
+                temp=temp->right;
+            }
+            else{
+                TreeNode* prv=temp->left;
+                while(prv->right&& prv->right!=temp){
+                    prv=prv->right;
+                }
+                    if(prv->right==NULL){
+                        prv->right=temp;
+                        temp=temp->left;
+                    }
+                    else{
+                        prv->right=NULL;
+                         count++;
+                    if(count==k){
+                    ans=temp->val;
+                        
+                    }
+                        temp=temp->right;
+                    }
+                
+            }
+        }
+        return ans;
+    } 
+    
+    
     
 };
