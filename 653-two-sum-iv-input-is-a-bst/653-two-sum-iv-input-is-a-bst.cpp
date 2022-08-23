3d4dfea83ceefec9eec67b389038c6b3d307e45f
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2022-08-24 00:45:48 +0530

    Time: 591 ms (5.08%), Space: 735.4 MB (5.26%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -14,7 +14,7 @@ public:
     
     // first we push all elements in vector in sorted order(bst inorder is always sorted) and then we just use 2 pointer 
     // brute force h ye time-O(N) space-O(N)
-    /*
+    
     vector<int> inorder;
     bool findTarget(TreeNode* root, int k) {
         
@@ -39,15 +39,28 @@ public:
         helper(root->right,k);
         return inorder;
     }
-    */
-    unordered_set<int>us;
+    
+    
+    // unordered set se kia h ye
+    //iska run time and space upr vale se bht better aa rhi h
+    
+/*    unordered_set<int>us;
     bool findTarget(TreeNode* root,int k){
         if(root==NULL) return false;
         
-        if(us.count(k-root->val)) return true;
+        if(us.count(k-root->val)) return true; // us has unique value so when we use count it will check if it has that value present or not the function return 1 if present 0 if not
+        // so if hm check kr rhe h k- root->val agr present h us mei to bhadia mtlb ans aagya ni to ye value push kro aur aage niklo bcoz there wont be any duplicates in bst to binakuch check kre kr skte h insert
         us.insert(root->val);
         return findTarget(root->left,k) || findTarget(root->right,k);
         
-    }
+    }*/
+    
+    //                                    OPTIMISED SOLUTION
+    
+ //   bool findTarget(TreeNode* root,int k){
+        
+ //   }
+    
+    
     
 };
