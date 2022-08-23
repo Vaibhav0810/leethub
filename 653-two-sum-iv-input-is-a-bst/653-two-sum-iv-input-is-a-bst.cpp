8e8d4c0e107f83024e82c882299ab26c96344311
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2022-08-24 00:29:31 +0530

    Time: 398 ms (5.08%), Space: 736.1 MB (5.26%) - LeetHub

---
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    // first we push all elements in vector in sorted order(bst inorder is always sorted) and then we just use 2 pointer 
+    
+    vector<int> inorder;
+    bool findTarget(TreeNode* root, int k) {
+        
+        helper(root,k);
+        
+        int i=0;
+        int j=inorder.size()-1;
+        
+        while(i<j){
+            if(inorder[i]+inorder[j]==k) return true;
+            else if(inorder[i]+inorder[j]<k) i++;
+            else if(inorder[i] +inorder[j]>k) j--;
+        }
+        return false;
+    }
+    
+    vector<int>helper(TreeNode* root,int k){
+        if(root==NULL) return inorder;
+        helper(root->left,k);
+        
+        inorder.push_back(root->val);
+        helper(root->right,k);
+        return inorder;
+    }
+    
+};
