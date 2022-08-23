ea75882e6032631f71b70500f40ead116798022e
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2022-08-23 17:20:14 +0530

    Time: 10 ms (43.96%), Space: 13.7 MB (75.54%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -9,6 +9,11 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+// we make upper bound here and we go to the left and keep on changing upper bound as root->val now when we go right we dont change upper bound and keep it same and then insert right
+
+// in short agr vector ki next value choti h to usse left mei lga do ni to right mei (agr bst ki property ko follow kr rhi h to)
+
 class Solution {
 public:
     TreeNode* bstFromPreorder(vector<int>& preorder) {
@@ -17,8 +22,8 @@ public:
     }
     
     TreeNode* help(vector<int>& preorder,int& i,int bound){
-        
-        if(i==preorder.size()||preorder[i]>bound) return NULL;
+        // if(preorder[i]> bound || i==preorder.size()) yye likhne me isilie error aarha h bcoz agr i size mei bda hoga to ye phle bound se check krega aur vo element array mei h hi ni index exceed kr rha h limit to overflow error dega iislie neeche vala shi h
+        if(i==preorder.size() || preorder[i]>bound) return NULL;
         
         TreeNode* root=new TreeNode(preorder[i++]);
         root->left=help(preorder,i,root->val);
