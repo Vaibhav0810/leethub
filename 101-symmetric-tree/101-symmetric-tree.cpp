c3d3d63ce60ab3beb7ae87418eeab9972fc39c9e
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2022-08-25 11:37:30 +0530

    Time: 14 ms (20.15%), Space: 16.9 MB (6.19%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.cpp
+++ 101-symmetric-tree/101-symmetric-tree.cpp
@@ -61,4 +61,35 @@ public:
         }
         return true;
     }
+    
+    /*
+                        USING ONE STACK(JAVA CODE)
+public boolean isSymmetric(TreeNode root) {
+  if (root == null) {
+    return true;
+  }
+  if (root.left == null && root.right == null) return true;
+  if (root.left == null || root.right == null) return false;
+  // children are not null
+  Stack<TreeNode> stack = new Stack<>();
+  stack.push(root.left);
+  stack.push(root.right);
+  
+  while (stack.size() > 0) {
+    TreeNode t1 = stack.pop();
+    TreeNode t2 = stack.pop();
+    // null check
+    if (t1 == null && t2 == null) continue;
+    if (t1 == null || t2 == null) return false;
+    // value check
+    if (t1.val != t2.val) return false;
+    // push children
+    stack.push(t1.right); stack.push(t2.left); // could be null
+    stack.push(t1.left); stack.push(t2.right);
+  }
+  
+  return true;
+}
+    */
+    
 };
