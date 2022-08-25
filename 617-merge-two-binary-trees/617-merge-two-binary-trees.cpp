ce8cf49ee2f9d56795b02db4eacb7f0849519499
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2022-08-25 12:32:42 +0530

    Time: 42 ms (89.61%), Space: 33 MB (24.08%) - LeetHub

--- 617-merge-two-binary-trees/617-merge-two-binary-trees.cpp
+++ 617-merge-two-binary-trees/617-merge-two-binary-trees.cpp
@@ -11,28 +11,52 @@
  */
 class Solution {
 public:
-   // Time and space=O(N)
-    // isme ye ho rha h phle  agr dono root null h to null return krdo
-    // agr ik null h to opposite return krdo
-    // now root1 mei root1+root2 daalenghe
-    // but jb root1 null hoga ye uddhr ni jaayega agr aise krenge to
-    //mergeTrees(root1->left,root2->left);(sirf aise krenge to ni hoga)
-    //bcoz vo null h udhr jaakr retuyrn kr dega
-    // isilie hm root1 ke left ya right ko uske eq krenge jiss se agr udhr null bhi ho to kuch bnn jaaye udhr
-    // 
-    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
-        if(root1==NULL && root2==NULL) return NULL;
-         if(root1==NULL) {
-             
-             return root2;
-         }
-         if(root2==NULL) {
-             return root1;
-         }
+    
+    // making a new tree
+    
+    TreeNode* help(TreeNode* root1,TreeNode* root2){
         
-        root1->val=root1->val+root2->val;
-        root1->left=mergeTrees(root1->left,root2->left);
-        root1->right=mergeTrees(root1->right,root2->right);
-        return root1;
+        if(root1 && root2){
+        
+        TreeNode* merged=new TreeNode(root1->val+root2->val);
+        
+        
+        merged->left=help(root1->left,root2->left) ;
+        merged->right=help(root1->right,root2->right);  
+            return merged;
+        }
+        else{
+            if(root1==NULL && root2!=NULL){
+                return root2;
+            }
+            else return root1;
+        }
+        
+        
     }
+    
+    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
+        
+        
+        
+         return help(root1,root2);
+        
+     }
+    
+    // merging in the same tree
+    
+    // https://leetcode.com/problems/merge-two-binary-trees/discuss/588123/~100.00-fast-in-run-time-and-memory-RecursiveIterativeBFSDFS
+  /*  
+    TreeNode* mergeTrees(TreeNode* t1,TreeNode *t2){
+        if(t1 && t2){
+            TreeNode* root=new TreeNode(t1->val+t2->val);
+            root->left=mergeTrees(t1->left,t2->left);
+            root->right=mergeTrees(t1->right,t2->right);
+            return root;
+        }
+        else{
+            return t1?t1:t2;
+        }
+    }
+    */
 };
