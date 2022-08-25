698a5842c262d8c3f4309ec5aaee3c9809c654d2
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2022-08-25 11:34:56 +0530

    Time: 3 ms (94.38%), Space: 16.6 MB (9.11%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.cpp
+++ 101-symmetric-tree/101-symmetric-tree.cpp
@@ -8,97 +8,57 @@
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
- */
+ 
+
 class Solution {
 public:
-    // iislie vector vala method use ni kr skte bcoz of this eg
     
-          //    1
-          // 2      2
-          //  3       3
-    // bcoz ye to hmare logic se shi ans hi dega
-    // so think diff logic
-  /*  
-    queue<TreeNode*>q;
-    bool check=false;
-    bool help(TreeNode* root){
-        q.push(root);
-        while(!q.empty()){
-            TreeNode* temp=q.front();
-            int n=q.size();
-            q.pop();
-            vector<int>v;
-            for(int i=0;i<n;i++){
-                v.push_back(temp->val);
-                if(temp->left) q.push(temp->left);
-                if(temp->right) q.push(temp->right);
-            }
-            int m=v.size();
-            for(int i=0;i<m;i++){
-                if(v[i]==v[m-i-1]){
-                    check=true;
-                }
-                else { 
-                check=false;      
-                return false;
-                }
-            }
-            if(check==false) return false;
-            
+    bool help(TreeNode* left,TreeNode* right){
+         if(left==NULL && right==NULL)   return true;
+        if(left==NULL || right==NULL)   return false;
+        if(left->val!=right->val){
+            return false;
         }
-        return check;
-        
+        else{
+            return help(left->left,right->right)&& help(left->right,right->left);
+        }
     }
     
     bool isSymmetric(TreeNode* root) {
-        return help(root);
+        
+        if(root==NULL) return 0;
+        return help(root->left,root->right);
+        
     }
-    */
-    // 2 option h ya to 2 queue bna le ya 2 pointer 2 pointer jyada space efficient h
-    
-    bool isSymmetric(TreeNode* root){
+};
+*/
+
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
         if(!root) return true;
-        queue<TreeNode*>q;
-        q.push(root->left);
-        q.push(root->right);
-        while(!q.empty()){
-            TreeNode* temp1=q.front();
-            q.pop();
-            TreeNode* temp2=q.front();
-            q.pop();
-             if(temp1 &&!temp2) return false;
-             if(!temp1 && temp2) return false;
-           // if(temp1==NULL || temp2==NULL) return false; // ye nii chl rha
-            if(temp1 && temp2){
-            if(temp1->val!=temp2->val) return false;
+        queue<TreeNode*>q1;
+        queue<TreeNode*>q2;
+        
+        q1.push(root->left);
+        q2.push(root->right);
+        
+        while(!q1.empty() && !q2.empty()){
+            TreeNode* left=q1.front();
+            TreeNode* right=q2.front();
+            q1.pop();
+            q2.pop();
             
-                q.push(temp1->left);
-                q.push(temp2->right);
-                q.push(temp1->right);
-                q.push(temp2->left);
+            if(left==NULL && right==NULL) continue;
+            else if(left==NULL || right==NULL) return false;
+            else if(left->val !=right->val) return false;
             
-            }
+            q1.push(left->left);
+            q1.push(left->right);
+            q2.push(right->right);
+            q2.push(right->left);
+            
         }
         return true;
     }
-    
-    // recursive( isme dekhenge agr left ki value h right ke ya ni agr null tk aate aate equal h to mtlb ans shi h)
-    // ni to beech mei return false 
-    
-//     bool help(TreeNode* left,TreeNode* right){
-//         if(left==NULL && right==NULL) return true;
-//         if(!left|| !right) return false;
-        
-//         if(left->val!=right->val){
-//             return false;
-//         }
-//         else{
-//             return help(left->left,right->right)&& help(left->right,right->left);
-//         }
-        
-//     }
-//     bool isSymmetric(TreeNode* root){
-//         return help(root->left,root->right);
-//     }
-    
 };
