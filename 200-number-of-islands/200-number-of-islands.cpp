86a110f6a6ad675092b0261a901cdf458d99a973
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2022-09-10 23:27:39 +0530

    Time: 728 ms (5.03%), Space: 396.3 MB (5.06%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -0,0 +1,78 @@
+    class Solution {
+    public:
+        int numIslands(vector<vector<char>>& grid) {
+
+            int n=grid.size();
+            int m=grid[0].size();
+            int count=0;
+            vector<vector<int>>visi(n,vector<int>(m,0));
+
+            for(int i=0;i<n;i++){
+                for(int j=0;j<m;j++){
+                    if(!visi[i][j] && grid[i][j]=='1'){
+                        count++;
+                        bfs(i,j,visi,grid);
+                    }
+                }
+            }
+            return count;
+        }
+
+        private:
+        void bfs(int row,int col,vector<vector<int>>&visi,vector<vector<char>>grid){
+            visi[row][col]=1;
+            queue<pair<int,int>>q;
+            q.push({row,col});
+            int n=grid.size();
+                int m=grid[0].size();
+
+            while(!q.empty()){
+                int row=q.front().first;
+                int col=q.front().second;
+                q.pop();
+
+
+                //traverse neighbours and mark them land
+                // ye sb iislie bcoz hm ksisi bhi direction mei check kr skte h diagonaly erticall horizontally
+                // so upr vala element hoga row -1 neeche  vala row +1 and same for column aise
+                for(int delrow=-1;delrow<=1;delrow++){
+                    if(delrow==-1){
+                        
+                        int delcol=0;
+                        int nrow=row+delrow;
+                        int ncol=col+delcol;
+                        // now cehck krna h vaalid h ya ni bcoz agr sbse upr h to hm usse ni bolskte ki tu aur upr ka check kr
+                        if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]=='1' && !visi[nrow][ncol]){
+                            visi[nrow][ncol]=1;
+                            q.push({nrow,ncol});
+                        }
+                    }
+                   else if(delrow==1){
+                        int delcol=0;
+                        int nrow=row+delrow;
+                        int ncol=col+delcol;
+                        // now cehck krna h vaalid h ya ni bcoz agr sbse upr h to hm usse ni bolskte ki tu aur upr ka check kr
+                        if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]=='1' && !visi[nrow][ncol]){
+                            visi[nrow][ncol]=1;
+                            q.push({nrow,ncol});
+                        }
+                    }
+                    else if(delrow==0){
+                        for(int delcol=-1;delcol<=1;delcol=delcol+2){
+                         
+                        int nrow=row+delrow;
+                        int ncol=col+delcol;
+                        // now cehck krna h vaalid h ya ni bcoz agr sbse upr h to hm usse ni bolskte ki tu aur upr ka check kr
+                        if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]=='1' && !visi[nrow][ncol]){
+                            visi[nrow][ncol]=1;
+                            q.push({nrow,ncol});
+                        }
+                    }
+                    }
+                    
+                }
+
+            }
+        }
+
+    };
