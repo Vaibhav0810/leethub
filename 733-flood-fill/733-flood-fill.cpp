8d20b0948d0d779b925f45171daf39b6a2f72a20
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2022-09-11 10:14:18 +0530

    Time: 19 ms (33.87%), Space: 14 MB (78.64%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -6,6 +6,8 @@ public:
          
          
         int oColor=image[sr][sc];
+         
+         // ye isilie bcoz agr oColor = newcolor h to kuch krne ki zroort ni h bs direct return krdo
          if(oColor==newColor)return image;
         int n=image.size();
         int m=image[0].size();
@@ -16,8 +18,12 @@ public:
     
     void dfs(int row,int col,vector<vector<int>> &image,int oColor,int color,int n,int m){
         
-        if(row<n && col<m && row>=0 && col>=0 && image[row][col]==oColor){
+        if(row<n && col<m && row>=0 && col>=0 && image[row][col]==oColor/*&&image[row][col]!=color ye case yha bhi lg skta*/){
+            
             image[row][col]=color;
+            
+            // ye sare constraint islie bcoz sirf 4 direction mei travel krna h aur jb aisa krenge to ye sb bnega
+            // like upr jaayenge to row-1 hoga and col same rhega aise hi sare alg alg h
             dfs(row-1,col,image,oColor,color,n,m);
             dfs(row,col-1,image,oColor,color,n,m);
             dfs(row,col+1,image,oColor,color,n,m);
