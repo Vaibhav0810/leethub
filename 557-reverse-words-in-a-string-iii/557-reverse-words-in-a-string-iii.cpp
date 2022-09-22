6654643931bcf84ea1a01fadd23c099d60adf88e
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2022-09-22 23:12:18 +0530

    Time: 18 ms (74.93%), Space: 11.6 MB (26.90%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -1,27 +1,62 @@
 class Solution {
 public:
-    string reverseWords(string s) {
+    // brute
+    
+//     string reverseWords(string s) {
+//         s.push_back(' ');
+//         stack<char>st;
+//         string ans="";
+//         string a="";
+//         for(int i=0;i<s.length();i++){
+//             if(s[i]!=' '){
+//                 st.push(s[i]);
+//             }
+//             else{
+//                 while(!st.empty()){
+//                     ans.push_back(st.top());
+//                     st.pop();
+//                 }
+                
+//                 a=a+ans;
+//                 a.push_back(' ');
+//                 ans="";
+//             }
+//         }
+//         a.pop_back();
+//         return a;
+        
+//    }
+    
+    // optimised  two pointer
+    // space- O(1)
+    string reverseWords(string s){
+        int i=0;
+        int j=0;
+        string ans;
         s.push_back(' ');
-        stack<char>st;
-        string ans="";
-        string a="";
-        for(int i=0;i<s.length();i++){
-            if(s[i]!=' '){
-                st.push(s[i]);
+        for(int k=0;k<s.length();k++){
+            if(s[k]!=' '){
+                ans.push_back(s[k]);
+                j++;
             }
             else{
-                while(!st.empty()){
-                    ans.push_back(st.top());
-                    st.pop();
+                j--;
+                while(i<j){
+                    char temp=ans[i];
+                    ans[i]=ans[j];
+                    ans[j]=temp;
+                    i++;
+                    j--;
                 }
+                ans.push_back(' ');
                 
-                a=a+ans;
-                a.push_back(' ');
-                ans="";
+                i=k+1;
+                j=k+1;
             }
         }
-        a.pop_back();
-        return a;
+        ans.pop_back();
+        return ans;
         
     }
+    
 };
