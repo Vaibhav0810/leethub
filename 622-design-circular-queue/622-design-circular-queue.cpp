fe4c6dcd8e5156a70d1010bb81f95e1d1bf12bcd
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2022-09-25 22:35:44 +0530

    Time: 28 ms (84.35%), Space: 16.9 MB (11.06%) - LeetHub

--- 622-design-circular-queue/622-design-circular-queue.cpp
+++ 622-design-circular-queue/622-design-circular-queue.cpp
@@ -1,5 +1,6 @@
 // time comp- O(N) bcoz erase in vector takes o(N) bcoz after erase we have to shift elements too
 // space -O(N)
+/*
 class MyCircularQueue {
 public:
     
@@ -53,6 +54,43 @@ public:
         else return false;
     }
 };
+*/
+//time -O(1)
+class MyCircularQueue {
+public:
+    MyCircularQueue(int k) {
+        data.resize(k);
+        maxSize = k;
+    }
+    bool enQueue(int val) {
+        if (isFull()) return false;
+        tail = (tail + 1) % maxSize;
+        data[tail] = val;
+        return true;
+    }
+    bool deQueue() {
+        if (isEmpty()) return false;
+        if (head == tail) head = 0, tail = -1;
+        else head = (head + 1) % maxSize;
+        return true;
+    }
+    int Front() {
+        return isEmpty() ? -1 : data[head];
+    }
+    int Rear() {
+        return isEmpty() ? -1 : data[tail];
+    }
+    bool isEmpty() {
+        return tail == -1;
+    }
+    bool isFull() {
+        return !isEmpty() && (tail + 1) % maxSize == head;
+    }
+private:
+    int maxSize, head = 0, tail = -1;
+    vector<int> data;
+};
+
 
 /**
  * Your MyCircularQueue object will be instantiated and called as such:
