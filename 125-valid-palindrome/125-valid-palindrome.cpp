f9c7c8b013c20db96910ceae237d49c714b89c2a
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2022-09-29 23:05:36 +0530

    Time: 3 ms (96.73%), Space: 7.4 MB (77.54%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -4,31 +4,52 @@ public:
         
         //stack<char>st;
         
-        string st2;
+//         string st2;
         
-        for(int i=0;i<s.length();i++){
-            if((s[i]>='a' && s[i]<='z') || (s[i]>='0' && s[i]<='9') ){
-                //st.push(s[i]);
-                st2.push_back(s[i]);
-            }
-            else if(s[i]>='A' && s[i]<='Z'){
-                //st.push(tolower(s[i]));
-                st2.push_back(tolower(s[i]));
-            }
-        }
-        int i=0;
-        int j=st2.length()-1;
+//         for(int i=0;i<s.length();i++){
+//             if((s[i]>='a' && s[i]<='z') || (s[i]>='0' && s[i]<='9') ){
+//                 //st.push(s[i]);
+//                 st2.push_back(s[i]);
+//             }
+//             else if(s[i]>='A' && s[i]<='Z'){
+//                 //st.push(tolower(s[i]));
+//                 st2.push_back(tolower(s[i]));
+//             }
+//         }
+//         int i=0;
+//         int j=st2.length()-1;
         
-        while(i<=j){
-            if(st2[i]!=st2[j]){
-                return false;
-            }
-            else{
+//         while(i<=j){
+//             if(st2[i]!=st2[j]){
+//                 return false;
+//             }
+//             else{
+//                 i++;
+//                 j--;
+//             }
+//         }
+        
+//         return true;
+//     }
+    
+    // without any space
+        // isalnum is an inbuilt function which check if its alphanumberic or not its in c and included in ctype.h header file
+        
+        int n=s.length();
+        int i=0,j=n-1;
+        
+        while(i<j){
+            while(i<j && !isalnum(s[i])){
                 i++;
+            }
+            while(i<j && !isalnum(s[j])){
                 j--;
             }
+            if(tolower(s[i++])!=tolower(s[j--])) return false;
         }
-        
         return true;
+        
     }
+
+    
 };
