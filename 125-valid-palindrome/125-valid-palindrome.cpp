e8da5346d5fe32d61d744ce8b91b44b9a8131074
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2022-09-29 22:45:23 +0530

    Time: 12 ms (38.29%), Space: 7.8 MB (26.63%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -2,29 +2,33 @@ class Solution {
 public:
     bool isPalindrome(string s) {
         
-        stack<char>st;
+        //stack<char>st;
         
         string st2;
         
         for(int i=0;i<s.length();i++){
             if((s[i]>='a' && s[i]<='z') || (s[i]>='0' && s[i]<='9') ){
-                st.push(s[i]);
+                //st.push(s[i]);
                 st2.push_back(s[i]);
             }
             else if(s[i]>='A' && s[i]<='Z'){
-                st.push(tolower(s[i]));
+                //st.push(tolower(s[i]));
                 st2.push_back(tolower(s[i]));
             }
         }
         int i=0;
+        int j=st2.length()-1;
         
-        while(!st.empty()){
-            if(st.top()==st2[i]){
-                st.pop();
-                i++;
+        while(i<=j){
+            if(st2[i]!=st2[j]){
+                return false;
             }
-            else return false;
+            else{
+                i++;
+                j--;
+            }
         }
+        
         return true;
     }
 };
