11587049ce7d674fada1a37ce323662cb9b48c73
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2022-10-13 00:38:06 +0530

    Time: 40 ms (51.97%), Space: 16.9 MB (53.18%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -16,18 +16,31 @@ public:
         //Optimised
         
         //O(N) and O(N)
-        unordered_map<int,int>mp;
+//         unordered_map<int,int>mp;
+//         for(int i=0;i<nums.size();i++){
+//             mp[nums[i]]++;
+//         }
+        
+//         for(auto it:mp){
+//             if(it.second==1){
+//                 return it.first;
+//             }
+//         }
+//         return 0;
+//     }
+        
+        
+        // More optimise
+        // refer notes
+        // we are taking xor here 
+        // xor of same number is 0 and opposite is 1 and xor of any number with 0 is that number 
+        // so jb hm xor lenge 5 ^ 1 ka to 4 aayega binary se krenge 
+        // eg 1
+        // 2 ^ 2 =0 0^1 =1
+        if(nums.size()==1) return nums[0];
         for(int i=0;i<nums.size();i++){
-            mp[nums[i]]++;
+            nums[i+1]=nums[i]^nums[i+1];
         }
-        
-        for(auto it:mp){
-            if(it.second==1){
-                return it.first;
-            }
-        }
-        return 0;
+        return nums[nums.size()-1];
     }
-        
-    
 };
