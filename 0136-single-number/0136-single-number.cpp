107ecc9daa886365d91e9ab767d5664a6153e437
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2022-10-13 00:29:16 +0530

    Time: 46 ms (40.02%), Space: 20.1 MB (19.24%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,14 +1,29 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
+        // brute O(nlog n)
+//         if(nums.size()==1) return nums[0];
         
-        if(nums.size()==1) return nums[0];
+//         sort(nums.begin(),nums.end());
         
-        sort(nums.begin(),nums.end());
+//         for(int i=0;i<nums.size();i=i+2){
+//             if(nums[i]!=nums[i+1]){
+//                 return nums[i];
+//             }
+//         }
+//         return 0;
         
-        for(int i=0;i<nums.size();i=i+2){
-            if(nums[i]!=nums[i+1]){
-                return nums[i];
+        //Optimised
+        
+        //O(N) and O(N)
+        unordered_map<int,int>mp;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
+        }
+        
+        for(auto it:mp){
+            if(it.second==1){
+                return it.first;
             }
         }
         return 0;
