51b5cf02d551a72b1faf210d93ca95c63fd3e514
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2022-10-13 09:54:57 +0530

    Time: 44 ms (5.17%), Space: 7.5 MB (92.32%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
@@ -13,9 +13,14 @@ public:
 //         node->val=node->next->val;
 //         node->next=node->next->next;
         
-        auto next=node->next;
-        *node=*next;
-        delete next;
+        // auto next=node->next;
+        // *node=*next;
+        // delete next;
+        // ye upr ko aise bhi likh skte h isme bs kuch ni h node del kr rhe h
+        ListNode* temp=node->next;
+        node->val=temp->val;
+        node->next=temp->next;
+        delete(temp);
         
     }
 };
