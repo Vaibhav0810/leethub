6f0333e5d75aee597de73f5e796d35fdccc20d0b
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2022-10-13 14:52:05 +0530

    Time: 23 ms (25.28%), Space: 7.6 MB (74.98%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -9,7 +9,33 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        if(head==NULL || head->next==NULL) return NULL;
+        if(head==NULL || head->next==NULL) return NULL; // base case
+        
+//         //If there is a cycle, return the entry location of the cycle
+
+// 2.1) L1 is defined as the distance between the head point and entry point
+
+// 2.2) L2 is defined as the distance between the entry point and the meeting point
+
+// 2.3) C is defined as the length of the cycle
+
+// 2.4) n is defined as the travel times of the fast pointer around the cycle When the first encounter of the slow pointer and the fast pointer
+
+// According to the definition of L1, L2 and C, we can obtain:
+
+// the total distance of the slow pointer traveled when encounter is L1 + L2
+
+// the total distance of the fast pointer traveled when encounter is L1 + L2 + n * C
+
+// Because the total distance the fast pointer traveled is twice as the slow pointer, Thus:
+
+// 2 * (L1+L2) = L1 + L2 + n * C => L1 + L2 = n * C => L1 = (n - 1) C + (C - L2)*
+
+// It can be concluded that the distance between the head location and entry location is equal to the distance between the meeting location and the entry location along the direction of forward movement.
+
+// So, when the slow pointer and the fast pointer encounter in the cycle, we can define a pointer "entry" that point to the head, this "entry" pointer moves one step each time so as the slow pointer. When this "entry" pointer and the slow pointer both point to the same location, this location is the node where the cycle begins.
+
+
         
         ListNode* slow=head;
         ListNode* fast=head;
