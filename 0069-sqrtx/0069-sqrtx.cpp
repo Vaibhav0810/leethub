593d2ac2b6b38ea15c912f6696a0f5665231b21f
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2022-10-23 00:32:47 +0530

    Time: 4 ms (59.65%), Space: 5.9 MB (25.10%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,16 +1,28 @@
 class Solution {
 public:
     long long mySqrt(int x) {
-        for(long long i=1;i<1000000;i++){
-            if(i*i==x){
-                return i;
-            }
-            else{
-                if(((i*i)<x) && ((i+1)*(i+1)>x)){
-                    return i;
-                } 
-            }
+        // brute
+        // for(long long i=1;i<1000000;i++){
+        //     if(i*i==x){
+        //         return i;
+        //     }
+        //     else{
+        //         if(((i*i)<x) && ((i+1)*(i+1)>x)){
+        //             return i;
+        //         } 
+        //     }
+        // }
+        // return 0;
+        
+        //optimal (newton method)
+        
+        if(x==0) return 0;
+        
+        long r=x;
+        while(r*r>x){
+            r=(r+x/r)/2;
         }
-        return 0;
+        return r;
+        
     }
 };
