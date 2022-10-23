9a93ab5a40b0f3317c1ac8bcd480ac3e0a23c69b
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2022-10-23 23:08:05 +0530

    Time: 127 ms (78.35%), Space: 53.4 MB (24.50%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,10 +1,28 @@
 class Solution {
 public:
-    bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i]==nums[i+1]) return true;
-        }
-        return false;
+    
+    // brute
+    // bool containsDuplicate(vector<int>& nums) {
+    //     sort(nums.begin(),nums.end());
+    //     for(int i=0;i<nums.size()-1;i++){
+    //         if(nums[i]==nums[i+1]) return true;
+    //     }
+    //     return false;
+    // }
+    
+    // optimised
+    // O(N) and O(N)
+    bool containsDuplicate(vector<int>& nums){
+    unordered_map<int,int>mp1;
+    for(auto it:nums){
+        mp1[it]++;
     }
+    
+    for(auto it:mp1){
+        if(it.second>1){
+            return true;
+        }
+    }
+    return false;
+        }
 };
