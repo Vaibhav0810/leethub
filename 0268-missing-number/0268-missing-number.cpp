cd462f39e9d0514827e36476ac6d04f4ea5a7d5e
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2022-10-24 00:20:24 +0530

    Time: 41 ms (52.51%), Space: 17.9 MB (64.88%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,21 +1,37 @@
 class Solution {
 public:
+    // brute
+    // int missingNumber(vector<int>& nums) {
+    //     vector<int>check;
+    //     sort(nums.begin(),nums.end());
+    //     for(int i=0;i<=nums.size();i++){
+    //         check.push_back(i);
+    //     }
+    //     bool shi=false;
+    //     for(int i=0;i<nums.size();i++){
+    //         if(check[i]!=nums[i]){
+    //             shi=true;
+    //             return check[i];
+    //         }
+    //     }
+    //     if(shi==false){
+    //         return check[check.size()-1];
+    //     }
+    //     return 0;
+    // }
+    
+    // optimised 
+    // time-O(N) space -O(1)
+    // using sum
+    
     int missingNumber(vector<int>& nums) {
-        vector<int>check;
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<=nums.size();i++){
-            check.push_back(i);
-        }
-        bool shi=false;
+        int n=nums.size()-1;
+        int sum1=((n+1)*(2*1+(n)*1))/2; //
+        int sum2=0;
         for(int i=0;i<nums.size();i++){
-            if(check[i]!=nums[i]){
-                shi=true;
-                return check[i];
-            }
+            sum2=sum2+nums[i];
         }
-        if(shi==false){
-            return check[check.size()-1];
-        }
-        return 0;
+        return sum1-sum2;
     }
+    
 };
