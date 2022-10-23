8cf366e01c4ab89611087109f057a5ba0db7bde3
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2022-10-23 21:42:30 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (87.20%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
@@ -1,5 +1,25 @@
 class Solution {
 public:
+    
+    // AA -> 26*1 +1 =27 (A==1)
+    // BA -> 26*2 +1 =53
+    // if three cases occur
+    // AAB -> A=1
+    // AA = 26*1 +1
+    // AAB = (26*1+1) * 26 +2
+    
+//     "B" = 2
+//     "BC" = (2)26 + 3
+//     "BCM" = (2(26) + 3)26 + 13
+    
+//     . For every additional digit of the string, we multiply the value of the digit by 26^n
+// 2. here n is the number of digits it is away from the one's place.
+// 3. This is similar to how the number 254 could be broken down as this:
+// 	 (2 x 10 x 10) + (5 x 10) + (4).
+// 4. The reason we use 26 instead of 10 is because 26 is our base.
+
+// For s = "BCM" the final solution would be (2 x 26 x 26) + (3 x 26) + (13)
+    
     int titleToNumber(string columnTitle) {
         int result=0;
         
@@ -9,4 +29,7 @@ public:
         }
         return result;
     }
+    
+    
+    
 };
