90aeb45fc067604bc1ce47e44b6747f2d47baf31
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2022-10-23 11:48:40 +0530

    Time: 8 ms (90.06%), Space: 7.4 MB (56.37%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -31,5 +31,22 @@ public:
     }
     return true;
     }
+//     Since the problem statement says that "the string contains only lowercase alphabets", we can simply use an array to simulate the unordered_map and speed up the code. The following implementation takes 12 ms.
+
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//         if (s.length() != t.length()) return false;
+//         int n = s.length();
+//         int counts[26] = {0};
+//         for (int i = 0; i < n; i++) { 
+//             counts[s[i] - 'a']++;
+//             counts[t[i] - 'a']--;
+//         }
+//         for (int i = 0; i < 26; i++)
+//             if (counts[i]) return false;
+//         return true;
+//     }
+// };
     
 };
