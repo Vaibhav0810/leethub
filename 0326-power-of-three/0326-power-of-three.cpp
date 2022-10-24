abb231cda20437725eeda131cfe3a6c56f3483e0
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2022-10-24 22:54:46 +0530

    Time: 31 ms (64.47%), Space: 6.2 MB (8.43%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        for(int i=0;i<n;i++){
+            if(pow(3,i)==n) return true;
+            else if(pow(3,i)>n) return false;
+        }
+        return false;
+    }
+};
