fd3482e535bbd112f8bbc293ef66bd76f367bcff
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2022-10-24 23:11:42 +0530

    Time: 44 ms (30.12%), Space: 5.9 MB (73.32%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -9,15 +9,30 @@ public:
     // }
     // doing it without pow
     // follow up solve without loops/ recursion
-    bool isPowerOfThree(int n){
-        if(n<=0) return false;
-        while(n>1){
-            if(n%3!=0) return false;
-            else{
-                n=n/3;
-            }
-        }
-        return true;
-    }
+//     bool isPowerOfThree(int n){
+//         if(n<=0) return false;
+//         while(n>1){
+//             if(n%3!=0) return false;
+//             else{
+//                 n=n/3;
+//             }
+//         }
+//         return true;
+//     }
+    
+//     3 is a prime number. So, any power of 3 will only be divisible by lower or equal powers of 3.
+
+// The largest power of 3 less than 2^31 is 1162261467 (can be found by 3^( log3(2^31 - 1) ) = 3^19 = 1162261467 ). So, this number will only be divisible by a power of 3.
+
+// Thus, we can only check if the above number is divisible by n. If n is a power of 3, then only it will be completely divisible by n.
+
+bool isPowerOfThree(int n) {
+	return n > 0 && 1162261467 % n == 0;
+	// pow(3, floor(log(INT_MAX)/log(3))) = 1162261467 
+}
+// Time Complexity : O(1)
+// Space Complexity : O(1)
+
+// Note: This solution worked because 3 is a prime number and will work for any other prime number power checks as well. It won't work for non-prime numbers because its largest power will also get divided by some non-powers of that number as well.
     
 };
