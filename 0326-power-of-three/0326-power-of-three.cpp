9d5dd792f7281d4801f143b51375c645e6e0cfd0
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2022-10-24 23:01:54 +0530

    Time: 61 ms (7.76%), Space: 5.8 MB (73.32%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,10 +1,23 @@
 class Solution {
 public:
-    bool isPowerOfThree(int n) {
-        for(int i=0;i<n;i++){
-            if(pow(3,i)==n) return true;
-            else if(pow(3,i)>n) return false;
+    // bool isPowerOfThree(int n) {
+    //     for(int i=0;i<n;i++){
+    //         if(pow(3,i)==n) return true;
+    //         else if(pow(3,i)>n) return false;
+    //     }
+    //     return false;
+    // }
+    // doing it without pow
+    // follow up solve without loops/ recursion
+    bool isPowerOfThree(int n){
+        if(n<=0) return false;
+        while(n>1){
+            if(n%3!=0) return false;
+            else{
+                n=n/3;
+            }
         }
-        return false;
+        return true;
     }
+    
 };
