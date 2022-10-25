4e76095ae5d26da9643af864c061973f6f00fc5e
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2022-10-25 11:30:16 +0530

    Time: 8 ms (59.56%), Space: 11.3 MB (89.94%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
@@ -1,16 +1,38 @@
 class Solution {
 public:
-    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
-        string w1="";
-        string w2="";
+//     bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+//         string w1="";
+//         string w2="";
         
-        for(int i=0;i<word1.size();i++){
-            w1=w1+word1[i];
+//         for(int i=0;i<word1.size();i++){
+//             w1=w1+word1[i];
+//         }
+//         for(int i=0;i<word2.size();i++){
+//             w2=w2+word2[i];
+//         }
+//         if(w1==w2) return true;
+//         return false;
+//     }
+    
+    // Optimised
+    // O(1) space
+    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2){
+        int i=0,j=0;
+        int m=0,n=0;
+        
+        while(i<word1.size() && j<word2.size()){
+            if(word1[i][m++]!=word2[j][n++]){
+                return false;
+            }
+            if(m>=word1[i].size()){
+                i++,m=0;
+            }
+            if(n>=word2[j].size()){
+                j++,n=0;
+            }
+            
         }
-        for(int i=0;i<word2.size();i++){
-            w2=w2+word2[i];
-        }
-        if(w1==w2) return true;
-        return false;
+        return i==word1.size() && j==word2.size();
     }
+    
 };
