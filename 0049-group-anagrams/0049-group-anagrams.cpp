5f502948d6fdf1d4ae1f9db1a29126e0f3bb74b1
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2022-10-25 13:48:17 +0530

    Time: 82 ms (40.80%), Space: 20.7 MB (41.72%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         
+        // isme ik map bna dia string and vector type ka ab hm string ko sort krenge aur uske bdd jin jin ka sort same hoga unko ik mei push kr denge 
+        // baaki ko alg mei aise pura sorted aajayega aur usse firr array mei push kr denge 
         unordered_map<string,vector<string>>mp1;
         
         for(auto s:strs){
