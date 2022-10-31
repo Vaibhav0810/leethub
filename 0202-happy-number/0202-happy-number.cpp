f625dbee744cb3e8f9ac9dbd8378d5fe1bcc1127
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2022-10-31 21:38:08 +0530

    Time: 3 ms (66.00%), Space: 5.8 MB (94.36%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -2,20 +2,47 @@ class Solution {
 public:
     // isme hashset mei number store krte chl rhe h aur jb number match ho jayega mtlb loop ho gya h to uss pr false return kr denge aur nhi to sb pura ho gya to true return
     
-bool isHappy(int n) {
-        set<int>s;
+// bool isHappy(int n) {
+//         unordered_set<int>s;
+//         int sum=0;
+//     while(sum!=1){
+//         sum=0;
+//         while(n>0){
+//             int x=n%10;
+//             sum=sum+(x*x);
+//             n=n/10;
+//         }
+//         if(s.find(sum)!=s.end()) return false;
+//         else s.insert(sum);
+//         n=sum;
+//     }
+//     return true;
+// }
+    
+    // using floyd tortoise and hair algo
+    // in this fast pointer double dpeed se chl rha h to agr vo chlte chlte slow pr aajta h to mtlb h ki loop h 
+    
+    int next(int n){
         int sum=0;
-    while(sum!=1){
-        sum=0;
         while(n>0){
             int x=n%10;
             sum=sum+(x*x);
             n=n/10;
         }
-        if(s.find(sum)!=s.end()) return false;
-        else s.insert(sum);
-        n=sum;
+        return sum;
     }
-    return true;
-}
+    
+    bool isHappy(int n){
+        int slow=next(n);
+        int fast=next(next(n));
+        
+        while(slow!=fast){
+            slow=next(slow);
+            fast=next(next(fast));
+            
+        }
+        return fast==1;
+        
+    }
+    
 };
