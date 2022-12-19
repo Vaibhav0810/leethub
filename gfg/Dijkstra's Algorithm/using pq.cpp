8ad416b6140902e6caef7221ebcde6d02e1a6c5f
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2022-12-19 22:10:22 +0530

    Update using pq.cpp

--- gfg/Dijkstra's Algorithm/using pq.cpp
+++ gfg/Dijkstra's Algorithm/using pq.cpp
@@ -5,6 +5,12 @@
 // basically hm isme ik min pq lete h aur firr bffs ki trh graph pr traverse krte h aur jo min dist hota h usse dist mei daalte rhte h aur queue mei weight
 // and node push krte rhte h min heap h to apne aap jo sbse min h vo starting mei aajati
 
+// isme negative isilie use ni ho skta because 
+// assume 2 nodes h 0 and 1 unke beech mei -2 dist h then 0 bolega mai udhr jaata hu km dist h (intitial dist=0) to vo udhr jaakr -2 dist krdega and then
+// firr 1 bolega mai udhr jaata hu vo -4 dist kr dega to isilie vo aise aise chlta rhega infinite
+
+//Time C - E log V. e= no of edges
+// v = no of nodes
 
 public:
 	//Function to find the shortest distance of all the vertices
