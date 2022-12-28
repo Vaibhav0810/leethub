f25f633eb42c29e8fdf38b940bc6975a22dedca1
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2022-12-28 23:24:31 +0530

    Time: 16 ms (90.06%), Space: 13.7 MB (48.40%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -22,43 +22,21 @@ public:
         // little optimised O(Nlog k)
         // using mapp and priority queue
         
-//         unordered_map<int,int>mp1;
-//         for(auto it:nums) mp1[it]++;
-//         vector<int>ans;
-//         priority_queue<pair<int,int>>pq;
+        unordered_map<int,int>mp1;
+        for(auto it:nums) mp1[it]++;
+        vector<int>ans;
+        priority_queue<pair<int,int>>pq;
         
-//         for(auto &it:mp1){
-//             pq.push({it.second,it.first});
-//         }
+        for(auto &it:mp1){
+            pq.push({it.second,it.first});
+        }
         
-//         while(!pq.empty() && k>0){
-//             int x=pq.top().second;
-//             ans.push_back(x);
-//             pq.pop();
-//             k--;
-//         }
-//         return ans;
-        unordered_map<int, int> frequency;
-  for (int num : nums) {
-    frequency[num]++;
-  }
-
-  // Create a bucket for each frequency
-  vector<vector<int>> buckets(nums.size() + 1);
-  for (auto& p : frequency) {
-    buckets[p.second].push_back(p.first);
-  }
-
-  // Extract the top k frequent elements from the buckets
-  vector<int> result;
-  for (int i = buckets.size() - 1; i >= 0 && result.size() < k; i--) {
-    for (int num : buckets[i]) {
-      result.push_back(num);
-      if (result.size() == k) {
-        break;
-      }
-    }
-  }
-  return result;
+        while(!pq.empty() && k>0){
+            int x=pq.top().second;
+            ans.push_back(x);
+            pq.pop();
+            k--;
+        }
+        return ans;
     }
 };
