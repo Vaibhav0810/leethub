e8f9f43655abceefab9765641868b92becdc9fb9
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2023-01-01 21:38:55 +0530

    Time: 33 ms (67.57%), Space: 25.2 MB (13.29%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        
+        // using extra space
+        // learned new concept suffix and prefix array
+        
+        // begi(prefix array)  A prefix sum array is another array prefixSum[]          of the same size, such that the value of prefixSum[i] is arr[0] +               arr[1] + arr[2] . . . arr[i].
+        
+        // Input: arr[] = {10, 20, 10, 5, 15}
+        //Output: prefixSum[] = {10, 30, 40, 45, 60}
+
+        
+        int n=nums.size();
+        
+        vector<int>begi(n),end(n);
+        begi[0]=1,end[0]=1;
+        
+        vector<int>ans(n);
+        
+        for(int i=1;i<nums.size();i++){
+            begi[i]=begi[i-1]*nums[i-1];
+            end[i]=end[i-1]*nums[n-i];
+        }
+        
+        for(int i=0;i<n;i++){
+            ans[i]=begi[i]*end[n-1-i];
+        }
+        return ans;
+    }
+};
