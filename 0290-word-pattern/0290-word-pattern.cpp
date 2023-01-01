9e21afc2c95837d48d4836f840d24da17e7e8856
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2023-01-01 14:45:10 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (82.12%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        unordered_map<string,char>mp1;
+        
+        
+        int x=0;
+        vector<string>v;
+        string c="";
+        while(x<s.size()){
+            while(s[x]!=' ' && x<s.size()){
+                c.push_back(s[x]);
+                x++;
+            }
+            v.push_back(c);
+            c.clear();
+            x++; 
+        }
+        set<char>count;
+        
+          if (v.size() != pattern.size())
+     {
+          return false;
+     }
+        
+        for(int i=0;i<pattern.size();i++){
+            if(mp1.find(v[i])==mp1.end() && count.find(pattern[i])==count.end()){
+                mp1.emplace(v[i],pattern[i]);
+                count.insert(pattern[i]);
+            }
+            else if(mp1[v[i]]!=pattern[i])
+                return false;
+        } 
+        return true;
+    }
+};
