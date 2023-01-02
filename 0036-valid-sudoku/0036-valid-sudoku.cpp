c673ca2670d78f62691607b0fc7ecee5ed491841
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2023-01-02 10:37:17 +0530

    Time: 36 ms (65.55%), Space: 20.4 MB (18.46%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,43 +1,86 @@
 class Solution {
 public:
-    bool checkRow(int row, int col, vector<vector<char>>& board){
-        for(int i = 0; i < 9; i++){
-            if(i == row) continue;
-            if(board[i][col] == board[row][col]) return false;
-        }
-        return true;
-    }
-    bool checkCol(int row, int col, vector<vector<char>>& board){
-        for(int i = 0; i < 9; i++){
-            if(i == col) continue;
-            if(board[row][i] == board[row][col]) return false;
-        }
-        return true;
-    }
-    bool checkGrid(int row, int col, vector<vector<char>>& board){
-        int startR = (row / 3) * 3, startC = (col / 3) * 3;
 
-        for(int i = 0; i < 9; i++){
-            int currR = startR + i/3, currC = startC + i%3;
-            if(currR == row && currC == col) continue;
-            if(board[currR][currC] == board[row][col]) return false;
-        }
-        return true;
-    }
-    bool isValidSudoku(vector<vector<char>>& board) {
+    
+     bool isValidSudoku(vector<vector<char>>& board) {
+//         int n=board.size();
+//         int m=board[0].size();
+//         set<char>st;
         
-        for(int i = 0; i < 9; i++){
-            for(int j = 0; j < 9; j++){
-                if(board[i][j] != '.'){
-                    bool rowClear = checkRow(i, j, board);
-                    bool colClear = checkCol(i, j, board);
-                    bool gridClear = checkGrid(i, j, board);
+//         for(int i=0;i<n;i++){
 
-                    if(!rowClear || !colClear || !gridClear) return false;
-                }
+//             for(int j=0;j<m-1;j++){
+//                 if(st.find(board[i][j])!=st.end() && board[i][j]!='.'){
+//                     return false;
+//                 }
+//                 else st.insert(board[i][j]);
+//             }
+//             st.clear();
+            
+//         }
+//         st.clear();
+        
+        
+//         for(int j=0;j<m;j++){
+//             for(int i=0;i<n-1;i++){
+//                 if(st.find(board[i][j])!=st.end() && board[i][j]!='.'){
+//                     return false;
+//                 }
+//                 else st.insert(board[i][j]);
+//             }
+//             st.clear();
+//         }
+//         st.clear();
+        
+//         int x=0;
+//         int xx=0;
+//         while(x<n){
+//             for(int i=x;i<x+3;i++){
+                
+//                 while(xx<m && i<x+3){
+                    
+//                 for(int j=xx;j<xx+3;j++){
+//                     if(st.find(board[i][j])!=st.end() && board[i][j]!='.'){
+//                         return false;
+//                     }
+//                     else st.insert(board[i][j]);
+//                 }
+//                     i++;
+//                     if(i==x+3){
+//                         xx+=3;
+//                         i=0;
+//                         st.clear();
+//                     }
+                    
+//                 }
+//                 st.clear();
+//                 break;
+//             }
+
+//             x+=3;
+//             xx=0;
+//             st.clear();
+//         }
+        
+//        return true;
+    
+        
+        vector<set<int>> rows(9), cols(9), blocks(9);
+     
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                
+                if(board[i][j]=='.') continue;
+                
+                int curr=board[i][j]-'0';
+                if(rows[i].count(curr) || cols[j].count(curr) || blocks[(i/3)*3+j/3].count(curr)) return false;
+                
+                rows[i].insert(curr);
+                cols[j].insert(curr);
+                blocks[(i/3)*3+j/3].insert(curr);
+                
             }
         }
-
         return true;
     }
 };
