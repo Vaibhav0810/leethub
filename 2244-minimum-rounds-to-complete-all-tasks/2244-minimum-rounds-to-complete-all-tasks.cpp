d3eabd86446f7099a81ffbe64690728532424313
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2023-01-04 23:53:30 +0530

    Time: 365 ms (53.51%), Space: 103.6 MB (72.00%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -41,6 +41,12 @@ public:
             
 //        }
         
+//         If the frequency of a number is only 1, then it is not possible to solve this question, so we return -1.
+// If the frequency is directly divided by 3, then we return the frequency/3.
+// If the remainder of dividing the number by 3 is 1 or 2 then, again there can be two cases:
+// If remainder is 1, i.e, lets take 7 frequency, then o o o o o o o the groups are something like this "ooo" "ooo" "o", i.e, one o is left alone so the solution is to break the second last group into two parts of 2 and 1 so that it becomes like: "ooo" "oo" "oo", i.e, one is added to the division by 3.
+// If remainder is 2, i.e, lets take 8 frequency, then o o o o o o o o the groups are something like this "ooo" "ooo" "oo", i.e, two o are left so the solution is to just add 1,i.e, one group of two to the solution.
+        
         for(auto it:mp1){
             if(it.second==1) return -1;
             
