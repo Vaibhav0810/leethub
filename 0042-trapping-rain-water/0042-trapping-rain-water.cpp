140c467fdecadaeb69f008bad80aab0366435a83
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2023-01-06 10:10:29 +0530

    Time: 30 ms (47.52%), Space: 19.7 MB (97.26%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        // brute force(tle)
+        
+        // isme jiss point pr h uski max legt height nikalo and then max right height
+        // aur jo unka min h usme se jiss height pr ho vo minus krke ans mei addd 
+        // bcoz agr ik container mei paani dalenge to vo utna hi paani store krega na
+        // jitna max height h uski kisi side se
+        
+//         int ans=0;
+//         for(int i=1;i<height.size()-1;i++){
+//             int l_height=0,r_height=0;
+            
+//             for(int j=i;j<height.size();j++) r_height=max(r_height,height[j]);
+            
+//             for(int j=i;j>=0;j--) l_height=max(l_height,height[j]);
+            
+//             ans+=min(l_height,r_height)-height[i];
+            
+//         }
+//         return ans;
+        
+        // using 2 pointer
+        
+        int left=0,right=height.size()-1,left_max=0,right_max=0,ans=0;
+        
+        while(left<right){
+            if(height[left]<height[right]){
+                height[left]>=left_max ? (left_max=height[left]) : ans+=(left_max-height[left]);
+                left++;
+            }
+            
+            else{
+                height[right]>=right_max ?  (right_max=height[right]):ans+=(right_max-height[right]);
+                    right--;
+            }
+            
+        }
+        return ans;
+    }
+};
