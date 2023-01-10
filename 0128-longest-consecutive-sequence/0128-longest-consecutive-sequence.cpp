18915434a0253acb016d3d95f6542f3143670552
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2023-01-10 18:52:33 +0530

    Time: 174 ms (76.32%), Space: 51.2 MB (23.26%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -2,29 +2,7 @@ class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
         
-        // brute force O(n log n)
-        
-//         if(nums.size()==0) return 0;
-        
-//         sort(nums.begin(),nums.end());
-//         int count=0;
-//         int maxi=0;
-//         for(int i=0;i<nums.size()-1;i++){
-//             if(nums[i]+1==nums[i+1]){
-//                 count++;
-//             }
-//             else if(nums[i]==nums[i+1]){
-//                 continue;
-//             }
-//             else{
-//                 maxi=max(maxi,count);
-//                 count=0;
-//             }
-//         }
-//         maxi=max(count,maxi);
-//         return maxi+1;
-        
-        // O(N)
+       // https://leetcode.com/problems/longest-consecutive-sequence/solution/
         
         set<int>st(nums.begin(),nums.end());
         if(nums.size()==0) return 0;
@@ -40,6 +18,9 @@ public:
             maxi=0;}
         }
         
-        return max(maxi,len)+1;;
+        return max(maxi,len)+1;
+        
     }
+    
+    
 };
