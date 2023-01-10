c109dc1bd3e2a764b59222c9b37e63ede0b5e444
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2023-01-10 18:58:08 +0530

    Update 0128-longest-consecutive-sequence.cpp

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -2,6 +2,28 @@ class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
         
+        // O(Nlog N)
+        
+         int longestConsecutive(vector<int>& nums) {
+        if(nums.size()==0) return 0;
+        sort(nums.begin(),nums.end());
+        int count=0;
+        int maxi=0;
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]+1==nums[i+1]){
+                count++;
+            }
+            else if(nums[i]==nums[i+1]){  // ye bcoz agr aisa h 1 1 2 3 4 to vo rest na kre phle one ke bdd
+                continue;
+            }
+            else{
+                maxi=max(maxi,count);
+                count=0;
+            }
+        }
+        maxi=max(count,maxi);
+        return maxi+1;
+        
        // https://leetcode.com/problems/longest-consecutive-sequence/solution/
         
         set<int>st(nums.begin(),nums.end());
