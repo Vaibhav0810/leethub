f15fd82c7ee9f04767da4e5bf6349a7915d21fce
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2023-01-11 11:28:41 +0530

    Time: 214 ms (86.57%), Space: 98.6 MB (76.68%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -2,6 +2,27 @@ class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temp) {
         
+                // brute force(tle)
+        // vector<int>ans(temp.size(),0);
+        // int n=temp.size();
+        // int count=0;
+        // for(int i=0;i<n-1;i++){
+        //     for(int j=i+1;j<n;j++){
+        //         if(temp[i]<temp[j]){
+        //             ans[i]=++count;
+        //             count=0;
+        //             break;
+        //         }
+        //         else{
+        //             count++;
+        //         }
+        //     }
+        //     count=0;
+        // }
+
+        // return ans;
+        
+        
         stack<int>st;
         vector<int>ans(temp.size(),0);
         
