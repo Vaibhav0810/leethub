0d4e7e0b05ac4190f396497a5b7c7165e4d5996b
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2023-01-12 22:26:48 +0530

    Time: 15 ms (84.24%), Space: 17.8 MB (6.75%) - LeetHub

--- 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.cpp
+++ 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.cpp
@@ -18,8 +18,17 @@ public:
     // follow up (log n)
     // we know for log n we have to do binary search
         
+        // lower bound return kr rha hjha pr phla 0 aayega aur agr 0 ni h usme to jha 0 se bda kuch bhi aayega
+        // jaise [-2,-1,-1,1,2,3] isme 3 return krega index
+        
+        // upper bound jha 0 ki last vaulue hogi ya jha 0 se bda hoga
+        // jaise [5,20,66,1314] isme 0 return krega
+        
+        // uske bdd unka max
+        
         int n = nums.size();
         int negStart = lower_bound(begin(nums), end(nums), 0) - nums.begin();
+        cout<<negStart;
         int posBegin = upper_bound(begin(nums), end(nums), 0) - nums.begin();        
         return max(negStart, (n  - posBegin ));
 
