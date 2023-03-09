43bbb94f218f1703bad0e469d3889cbbb6971276
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2023-03-09 22:21:44 +0530

    Time: 7 ms (86.54%), Space: 7.8 MB (29.86%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -9,50 +9,47 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        if(head==NULL || head->next==NULL) return NULL; // base case
         
-//         //If there is a cycle, return the entry location of the cycle
-
-// 2.1) L1 is defined as the distance between the head point and entry point
-
-// 2.2) L2 is defined as the distance between the entry point and the meeting point
-
-// 2.3) C is defined as the length of the cycle
-
-// 2.4) n is defined as the travel times of the fast pointer around the cycle When the first encounter of the slow pointer and the fast pointer
-
-// According to the definition of L1, L2 and C, we can obtain:
-
-// the total distance of the slow pointer traveled when encounter is L1 + L2
-
-// the total distance of the fast pointer traveled when encounter is L1 + L2 + n * C
-
-// Because the total distance the fast pointer traveled is twice as the slow pointer, Thus:
-
-// 2 * (L1+L2) = L1 + L2 + n * C => L1 + L2 = n * C => L1 = (n - 1) C + (C - L2)*
-
-// It can be concluded that the distance between the head location and entry location is equal to the distance between the meeting location and the entry location along the direction of forward movement.
-
-// So, when the slow pointer and the fast pointer encounter in the cycle, we can define a pointer "entry" that point to the head, this "entry" pointer moves one step each time so as the slow pointer. When this "entry" pointer and the slow pointer both point to the same location, this location is the node where the cycle begins.
-
-
+        // O(N)
+//         set<ListNode*> st;
+//         ListNode* slow=head;
+//         while(slow){
+            
+//             if(st.find(slow)==st.end()){
+//                 st.insert(slow);
+//                 slow=slow->next;
+//             }
+//             else{
+//                 return slow;
+//             }
+//         }
+//         return NULL;
         
-        ListNode* slow=head;
-        ListNode* fast=head;
-        ListNode* entry=head;
+        //O(1)
+        
+        
+        // ye iska logic ye h
+        // fast double speed se chlkr vha phcha h jha cycle detect hui h
+        // ab ye soch ki slow kuch tha hi ni aur fast 1 speed se chl rha tha
+        // to vo half distance pr h ab jha pr h 
+        // to ab slow ko aur fast ko 1 speed se jha vo h vhhha jaane ke lie same tym lgega
+        // isilie ab jb bdd mei usse head se chlaenge to same vha vo ussi area pr match krenge jha se cycle start hogi
+        
+       ListNode* slow=head,*fast=head;
+        
         
         while(fast && fast->next){
             slow=slow->next;
             fast=fast->next->next;
-            if(slow==fast){
-                while(slow!=entry){
-                    slow=slow->next;
-                    entry=entry->next;
-                }
-                return entry;
-            }
+            if(slow==fast) break;
         }
         
-        return NULL; // if no cycle found
+        if(fast==NULL || fast->next==NULL) return NULL;
+        
+        while(head!=slow){
+            head=head->next;
+            slow=slow->next;
+        }
+        return head;
     }
 };
