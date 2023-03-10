d7df36c7db1db5b065a33b59f25288184dd1458d
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2023-03-11 00:37:00 +0530

    Time: 24 ms (63.50%), Space: 16.5 MB (85.75%) - LeetHub

--- 0382-linked-list-random-node/0382-linked-list-random-node.cpp
+++ 0382-linked-list-random-node/0382-linked-list-random-node.cpp
@@ -10,18 +10,40 @@
  */
 class Solution {
 public:
-    vector<ListNode*>v;
-    Solution(ListNode* head) {
-        while(head){
-            v.push_back(head);
-            head=head->next;
-        }
-    }
+//     vector<ListNode*>v;
+//     Solution(ListNode* head) {
+//         while(head){
+//             v.push_back(head);
+//             head=head->next;
+//         }
+//     }
     
-    int getRandom() {
-        int i=rand()%v.size();
-        return v[i]->val;
-    }
+//     int getRandom() {
+//         int i=rand()%v.size();
+//         return v[i]->val;
+//     }
+    
+    // without extra space by calculating size
+    int n=0;
+    ListNode* temp=NULL;
+        Solution(ListNode* head){
+            temp=head;
+            while(head){
+                n++;
+                head=head->next;
+            }
+        }
+    
+        int getRandom(){
+            int i=rand()%n;
+            ListNode* p=temp;
+            while(i>0){
+                p=p->next;
+                i--;
+            }
+            return p->val;
+        }
+    
 };
 
 /**
