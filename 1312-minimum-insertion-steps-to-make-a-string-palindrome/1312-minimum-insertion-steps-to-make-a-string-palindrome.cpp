d520a6855adf7cf5b5ae6aa5e3cf5e6ac7e15c28
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2023-04-22 14:03:21 +0530

    Time: 33 ms (91.33%), Space: 7.6 MB (78.35%) - LeetHub

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int minInsertions(string s) {
+        
+        // f why am i soo dumb
+        
+        int n=s.size();
+        if(n==0) return 0;
+        string rev=s;
+        reverse(rev.begin(),rev.end());
+        int dp[n+1][n+1];
+        
+        for(int i=0;i<n+1;i++){
+            for(int j=0;j<n+1;j++){
+                if(i==0 || j==0) dp[i][j]=0;
+            }
+        }
+    
+            
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(s[i-1]==rev[j-1]){
+                    dp[i][j]=dp[i-1][j-1]+1;
+                }
+                else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return n-dp[n][n];
+    }
+};
