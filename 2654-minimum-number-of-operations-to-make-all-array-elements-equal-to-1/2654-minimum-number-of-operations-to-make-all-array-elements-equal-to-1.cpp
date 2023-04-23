cddeb4fd1b7bfdfe236181f8d5a4fb046e873462
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2023-04-23 14:39:51 +0530

    Time: 12 ms (13.64%), Space: 27.6 MB (18.18%) - LeetHub

---
+++ 2654-minimum-number-of-operations-to-make-all-array-elements-equal-to-1/2654-minimum-number-of-operations-to-make-all-array-elements-equal-to-1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int c=0;
+        for(auto it:nums){
+            if(it==1) c++;
+        }
+        int n=nums.size();
+        if(c>0) return n-c;
+
+
+        int res=1e7;        // just a dummy value
+        for(int i=0;i<n;i++){
+            int g=nums[i];
+            for(int j=i+1;j<n;j++){
+                g=__gcd(g,nums[j]);
+                if(g==1){
+                    res=min(res,j-i+(n-1)); // number of operations to make this element 1+ number of non ones (i.e. n-1) .
+                    break;
+                } 
+            }
+        }
+        return res==1e7?-1:res;
+    }
+};
