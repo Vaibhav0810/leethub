0c201831e355b9045f8f8cb31c77b38e530f567f
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2023-04-25 11:00:39 +0530

    Time: 72 ms (93.56%), Space: 35.4 MB (92.24%) - LeetHub

--- 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.cpp
+++ 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.cpp
@@ -1,32 +1,57 @@
 class SmallestInfiniteSet {
 public:
-    priority_queue<int,vector<int>,greater<int>>pq;
-    set<int>st;
     
-    SmallestInfiniteSet() {
-        for(int i=1;i<=1000;i++) {
-            pq.push(i); 
-            st.insert(i);
-        }
+    // with pq and set
+//      priority_queue<int,vector<int>,greater<int>>pq;
+//     set<int>st;
+    
+//     SmallestInfiniteSet() {
+//         for(int i=1;i<=1000;i++) {
+//             pq.push(i); 
+//             st.insert(i);
+//         }
+//     }
+    
+//     int popSmallest() {
+//         int x=pq.top();
+//         pq.pop();
+//         st.erase(x);
+//         return x;
+//     }
+    
+//     void addBack(int num) {
+//         if(st.find(num)==st.end()){
+//             pq.push(num);
+//             st.insert(num);
+//         }
+//         else{
+            
+//         }
+         
+//     }
+    
+    // with only set
+    
+    int cur = 1;
+    set<int> added; 
+    
+    SmallestInfiniteSet(){
+        
     }
     
     int popSmallest() {
-        int x=pq.top();
-        pq.pop();
-        st.erase(x);
-        return x;
+        if (!added.empty()) {
+            int res = *begin(added);
+            added.erase(begin(added));
+            return res;
+        }
+        return cur++;
+    }
+    void addBack(int num) {
+        if (num < cur)
+            added.insert(num);
     }
     
-    void addBack(int num) {
-        if(st.find(num)==st.end()){
-            pq.push(num);
-            st.insert(num);
-        }
-        else{
-            
-        }
-         
-    }
 };
 
 /**
