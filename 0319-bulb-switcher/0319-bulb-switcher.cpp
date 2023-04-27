0e8fc2cb6878d3a73dbbcdca7e34ba53ee6173f1
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2023-04-27 21:11:17 +0530

    Time: 0 ms (100.00%), Space: 6 MB (63.99%) - LeetHub

--- 0319-bulb-switcher/0319-bulb-switcher.cpp
+++ 0319-bulb-switcher/0319-bulb-switcher.cpp
@@ -4,14 +4,22 @@ public:
     
     
     int bulbSwitch(int n) {
-        if(n==0) return 0;
-        int sum=1;
         
-        for(long long i=2;i<=n/2;i++){
-            long long prod=i*i; 
-            if(prod<=n) sum++;
-        }
+        // brute force
         
-        return sum;
+//         if(n==0) return 0;
+//         int sum=1;
+        
+//         for(long long i=2;i<=n/2;i++){
+//             long long prod=i*i; 
+//             if(prod<=n) sum++;
+//         }
+        
+//         return sum;
+        
+        //optimal
+        
+        return sqrt(n);
+        
     }
 };
