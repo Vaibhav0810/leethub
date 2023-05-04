e9b4a75628c6a4ba4a97a2ebc1148daa5a89e005
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2023-05-04 23:17:50 +0530

    Time: 13 ms (24.81%), Space: 8 MB (37.02%) - LeetHub

---
+++ 0649-dota2-senate/0649-dota2-senate.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string predictPartyVictory(string senate) {
+        
+        queue<int>rq,dq;
+        int n=senate.size();
+        for(int i=0;i<n;i++){
+            if(senate[i]=='R') rq.push(i);
+            else dq.push(i);
+        }
+        
+        while(rq.size()!=0 && dq.size()!=0){
+            int fr=rq.front();
+            int fd=dq.front();
+            rq.pop();
+            dq.pop();
+
+            fr<fd ? rq.push(fr+n) : dq.push(fd+n);
+            
+            
+            
+        }
+        
+        if(rq.size()>dq.size()) return "Radiant";
+        else return "Dire";
+        
+    }
+};
