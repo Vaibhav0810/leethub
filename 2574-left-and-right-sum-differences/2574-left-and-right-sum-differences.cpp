ace4bd3ef26289d5d517e2c32c8c014af19a2f38
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2024-07-10 22:34:42 +0530

    Time: 3 ms (90.54%), Space: 13.4 MB (86.37%) - LeetHub

--- 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.cpp
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.cpp
@@ -9,17 +9,12 @@ public:
             if(i==0) continue;
             left[i]=nums[i-1]+left[i-1];
         }
-        for(auto it:left){
-            cout<<it<<" ";
-        }
         
         for(int i=n-1;i>=0;i--){
             if(i==n-1) continue;
             right[i]=nums[i+1]+right[i+1];
         }
-        for(auto it:right){
-            cout<<it<<" ";
-        }
+        
         
         vector<int>ans(n,0);
         for(int i=0;i<n;i++){
