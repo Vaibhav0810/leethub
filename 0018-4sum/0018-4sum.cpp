ebc584b33f66f10b6458f2511990950b20ecc1ba
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2024-12-01 01:00:30 +0530

    Time: 2321 ms (5.01%), Space: 570.6 MB (5.00%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,49 +1,39 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        sort(nums.begin(),nums.end());
-        vector<vector<int>>ans;
-        if(nums.size()<3){
-            return {};
-        }
-        for(int i=0;i<nums.size()-3;i++){
-            if(i>0 && nums[i]==nums[i-1]) continue;
-            for(int j=i+1;j<nums.size()-2;j++){
-                if(j>i+1 && nums[j]==nums[j-1]) continue;
-                
-                int k = j+1;
-                int l = nums.size()-1;
-                while(k<l){
-                long long sum = nums[i];
-                    sum+=nums[j];
-                    sum+=nums[k];
-                    sum+=nums[l];
-                
-                if(sum==target){
-                    vector<int>check;
-                    check.push_back(nums[i]);
-                    check.push_back(nums[j]);
-                    check.push_back(nums[k]);
-                    check.push_back(nums[l]);
-                    
+        sort(nums.begin(), nums.end());
+vector<vector<int>> ans;
+        set<vector<int>>st;
+        
+        if(nums.size()<4) return {};
+
+for (int i = 0; i < nums.size() - 2; i++) {
+    for (int j = i + 1; j < nums.size() - 1; j++) {
+        unordered_set<long long> mp1;
+        for (int k = j + 1; k < nums.size(); k++) {
+            vector<int> check;
+            long long sum = nums[i] + nums[j];
+                sum += nums[k];
+                long long fourth = target - sum;
+            if (mp1.find(fourth) == mp1.end()) {
+                mp1.insert(nums[k]);
+            } else {
+                check.push_back(nums[i]);
+                check.push_back(nums[j]);
+                check.push_back(nums[k]);
+                check.push_back((int)fourth);
+                if(st.find(check)==st.end()){
                     ans.push_back(check);
-                    k++;
-                    l--;
-                    while(k<l &&nums[k]==nums[k-1])k++;
-                    while(k<l && nums[l]==nums[l+1])l--;
-                    
+                    st.insert(check);
                 }
                 else{
-                    if(sum>target){
-                        l--;
-                    }
-                    else k++;
+                    
                 }
-                }
-                
             }
         }
-        return ans;
-        
     }
+}
+return ans;
+    }
+
 };
