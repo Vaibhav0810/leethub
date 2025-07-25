c32515cb8bef76ad9add8aa4d7cb9eff0ccae0e1
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2025-07-25 23:55:50 +0530

    Time: 278 ms (5.07%), Space: 83.2 MB (5.25%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
@@ -5,36 +5,35 @@ public:
 
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int V = grid.size();
-        queue<pair<int, int>> q;
-        if (grid[0][0] == 1 || grid[V - 1][V - 1] == 1)
-            return -1;
-        q.push({0, 0});
-        grid[0][0] = 1;
-        int count = 0;
-        while (!q.empty()) {
-            int N = q.size();
-            while (N--) {
-                auto curr = q.front();
-                q.pop();
-                int row = curr.first;
-                int col = curr.second;
-                if(row==V-1 && col ==V-1) return count+1;
-                for (auto dir : directions) {
-                    int nr = dir[0];
-                    int nc = dir[1];
 
-                    if (row + nr >= 0 && row + nr < V && col + nc >= 0 &&
-                        col + nc < V) {
-                        if (grid[row + nr][col + nc] == 0) {
-                            grid[row + nr][col + nc] = 1;
-                            q.push({row + nr, col + nc});
-                        }
-                    }
+        if(grid[0][0]==1 || grid[V-1][V-1]==1) return -1;
+        if(V-1==0) return 1;
+        vector<vector<int>>result(V,vector<int>(V,INT_MAX));
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
+        pq.push({0,{0,0}});
+        grid[0][0]=1;
+        result[0][0]=1;
+        int count=0;
+        while(!pq.empty()){
+            auto curr=pq.top();
+            pq.pop();
+            int dist=curr.first;
+            int row=curr.second.first;
+            int col=curr.second.second;
+            // if(row==V-1 && col==V-1) return result[row][col]+1;
+            for(auto dir:directions){
+                int nr=row+dir[0];
+                int nc=col+dir[1];
+                int ans=1;
+                if(nr>=0 && nr<V && nc>=0 && nc<V && grid[nr][nc]==0 && dist+ans<result[nr][nc]){
+                    result[nr][nc]=dist+1;
+                    // grid[nr][nc]=1;
+                    pq.push({result[nr][nc],{nr,nc}});
                 }
-                
             }
-            count++;
         }
+        if(result[V-1][V-1]==INT_MAX)
         return -1;
+        else return result[V-1][V-1]+1;
     }
 };
