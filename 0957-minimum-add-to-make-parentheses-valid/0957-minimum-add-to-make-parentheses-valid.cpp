731747483edb5a7cd1a7712911b26d6fa0daa85f
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2025-07-31 11:12:54 +0530

    Time: 0 ms (100%), Space: 8.2 MB (95.09%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
 
     // in this we will calculate how many open are there and how many close are there 
-    // and just return their difference
+    // and if open is greater than 0 and we find close we will minus it bcoz its a valid pair
+    // if open is already 0 then we are gonna need that much open as we have close so we will increament close and return open and close sum;\
 
     int minAddToMakeValid(string s) {
         int open=0,close=0;
