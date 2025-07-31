ab641e5bf3f500011a6173923cfbac69b29f395d
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2025-07-31 12:14:40 +0530

    Time: 3 ms (69.9%), Space: 10.9 MB (67.69%) - LeetHub

---
+++ 1197-parsing-a-boolean-expression/1197-parsing-a-boolean-expression.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    bool parseBoolExpr(string exp) {
+        int f =0,t=0,open=0,close=0,i=0,n=exp.length();
+        if(n<=1) return exp[0]=='f' ? false:true;
+        stack<char>st;
+        int currF=0,currT=0;
+        // st.push(exp[0]);
+        while(i<n){
+            if(exp[i]==')'){ 
+                close++; 
+                open--;
+                while(st.top()!='&' && st.top()!='!' && st.top()!='|'){
+                    
+                    if(st.top()=='f') f++;
+                    if(st.top()=='t') t++;
+                    st.pop();
+                }
+                if(st.top()=='&'){
+                    st.pop();
+                    if(t==0) st.push('f');
+                    else if(f==0) st.push('t');
+                    else if(f>0) st.push('f');
+                    else if(t>0) st.push('t');
+                }
+                else if(st.top()=='|'){
+                    st.pop();
+                    if(t>0) st.push('t');
+                    else st.push('f');
+                }
+                else{
+                    st.pop();
+                    if(t>0) st.push('f');
+                    else st.push('t');
+                }
+                f=0;
+                t=0;
+            }
+            
+            st.push(exp[i]);
+            i++;
+        }
+       
+        st.pop();
+        if(st.top()=='f') return false;
+        return true;
+    }
+};
