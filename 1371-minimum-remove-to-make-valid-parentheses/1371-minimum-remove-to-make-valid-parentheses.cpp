d6deb95cd6a0e5a77bebc5835ed9c3400e032708
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2025-08-04 11:48:25 +0530

    Time: 0 ms (100%), Space: 13.3 MB (88.52%) - LeetHub

--- 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.cpp
+++ 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.cpp
@@ -1,41 +1,70 @@
 class Solution {
 public:
+    // Time - O(k.n) where k is the number of invalid paranthesis Space O(1)
+    // In this we will check if ) exceeds ( this count then erase
+    // then in other pass we will check if ( exceeds ) this count then erase
+
+    // string minRemoveToMakeValid(string s) {
+        // int count = 0;
+        // int idx = 0;
+        // while (idx < s.length()) {
+        //     if (s[idx] == '(')
+        //         count++;
+        //     if (s[idx] == ')')
+        //         count--;
+
+        //     if ((s[idx] == ')' || s[idx] == '(') && count < 0) {
+        //         s.erase(s.begin() + idx);
+        //         count++;
+        //         idx--;
+        //     }
+        //     idx++;
+        // }
+        // idx = s.length()-1;
+        // if (count > 0) {
+        //     count = 0;
+        //     while (idx >=0) {
+        //         if (s[idx] == '(')
+        //             count--;
+        //         if (s[idx] == ')')
+        //             count++;
+
+        //         if ((s[idx] == ')' || s[idx] == '(') && count < 0) {
+        //             s.erase(s.begin() + idx);
+        //             count++;
+        //         }
+        //         idx--;
+        //     }
+        // }
+        // return s;
+    // }
+
+    // Time - > O(N) Space O(N)
+    // as we know there will be only lower case letter and brackets so we can mark extra bracket by a *
+    //  and then remove it
     string minRemoveToMakeValid(string s) {
-        int count = 0;
-        int idx = 0;
-        // string ans="";
-        while (idx < s.length()) {
-            if (s[idx] == '(')
+        int n=s.length();
+        string ans="";
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(s[i]=='('){
                 count++;
-            if (s[idx] == ')')
-                count--;
-
-            if ((s[idx] == ')' || s[idx] == '(') && count < 0) {
-                s.erase(s.begin() + idx);
-                count++;
-                idx--;
             }
-            // else ans.push_back(s[i]);
-            idx++;
+            if(s[i]==')') count--;
+            if(count<0) s[i]='*',count++;
         }
-        idx = s.length()-1;
-        if (count > 0) {
-            count = 0;
-            while (idx >=0) {
-                if (s[idx] == '(')
-                    count--;
-                if (s[idx] == ')')
-                    count++;
-
-                if ((s[idx] == ')' || s[idx] == '(') && count < 0) {
-                    s.erase(s.begin() + idx);
-                    count++;
-                    // idx++;
-                }
-                // else ans.push_back(s[i]);
-                idx--;
+        if(count>0){
+            count=0;
+            for(int i=n-1;i>=0;i--){
+                if(s[i]=='(') count--;
+                if(s[i]==')') count++;
+                if(count<0) s[i]='*',count++;
             }
         }
-        return s;
+        for(int i=0;i<n;i++){
+            if(s[i]!='*') ans.push_back(s[i]);
+        }
+        return ans;
+
     }
 };
