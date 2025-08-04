ed04ba548d03af2747a14dd199030b81291b83c3
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2025-08-04 14:43:04 +0530

    Time: 1 ms (78.01%), Space: 11.8 MB (22.23%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.cpp
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -1,5 +1,11 @@
 class Solution {
 public:
+
+    // Time - O(N), Space - O(N)
+    // isme hme jb bhi bigger element milega hm usse stack se pop kr denge n k-- kr denge
+    // firr bhi agr k = 0 ni hua to end se pop krna shuru kr denge
+    // then ans ko ans mei store krke return
+
     string removeKdigits(string num, int k) {
         stack<char> st;
         int n = num.length();
@@ -23,11 +29,12 @@ public:
         }
         reverse(ans.begin(), ans.end());
 
+        // to check aage ke zeroes
         while (ans[i] == '0') {
             i++;
         }
-
         ans = ans.substr(i);
+        // if string is empty return 0 or else return the string
         return ans.empty() ? "0" : ans;
     }
 };
