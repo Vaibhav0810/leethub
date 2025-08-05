528b6f7930d850ac3ac02c1e8749bb47daaa37d5
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2025-08-05 14:44:40 +0530

    Time: 24 ms (76.89%), Space: 23.5 MB (74.25%) - LeetHub

--- 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.cpp
+++ 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.cpp
@@ -1,5 +1,51 @@
 class Solution {
 public:
+
+    // Time - O(N) Space - O(N)
+
+    // int maximumGain(string s, int x, int y) {
+    //     int maxi=x>=y?x:y;
+    //     stack<char>st;
+    //     int ans=0;
+    //     if(maxi==x){
+    //         for(int i=0;i<s.size();i++){
+    //             if(!st.empty() && st.top()=='a' && s[i]=='b') st.pop(),ans+=x;
+    //             else st.push(s[i]);
+    //         }
+            
+    //         s.clear();
+    //         while(!st.empty()){
+    //             s.push_back(st.top());
+    //             st.pop();
+    //         }
+    //         reverse(s.begin(),s.end());
+    //         for(int i=0;i<s.size();i++){
+    //             if(!st.empty() && st.top()=='b' && s[i]=='a') st.pop(),ans+=y;
+    //             else st.push(s[i]);
+    //         }
+    //     }
+    //     else{
+    //         for(int i=0;i<s.size();i++){
+    //             if(!st.empty() && st.top()=='b' && s[i]=='a') st.pop(),ans+=y;
+    //             else st.push(s[i]);
+    //         }
+    //         s.clear();
+    //         while(!st.empty()){
+    //             s.push_back(st.top());
+    //             st.pop();
+    //         }
+    //         reverse(s.begin(),s.end());
+    //         for(int i=0;i<s.size();i++){
+    //             if(!st.empty() && st.top()=='a' && s[i]=='b') st.pop(),ans+=x;
+    //             else st.push(s[i]);
+    //         }
+
+    //     }
+    //     return ans;
+
+    // }
+
+    // Time - O(N) Space -O(1)
      int maximumGain(string s, int x, int y) {
         int n     = s.length();
         int score = 0;
