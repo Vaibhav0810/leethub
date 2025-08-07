3c536acab26dc685559bc8567242bb06cc11942b
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2025-08-07 17:13:11 +0530

    Time: 19 ms (16.9%), Space: 21.1 MB (13.07%) - LeetHub

--- 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.cpp
+++ 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.cpp
@@ -3,12 +3,10 @@ public:
     vector<vector<int>> ans;
     void dfs(int i, vector<int>& visi, vector<vector<int>>& graph,
              vector<int> temp, int n) {
-        // visi[i]=1;
         temp.push_back(i);
         if (i == n - 1)
             ans.push_back(temp);
         for (auto it : graph[i]) {
-            // temp.push_back(it);
             dfs(it, visi, graph, temp, n);
         }
     }
@@ -17,7 +15,6 @@ public:
         int n = graph.size();
         vector<int> visi(n, 0);
         vector<int> temp;
-        // temp.push_back(0);
         dfs(0, visi, graph, temp, n);
         return ans;
     }
