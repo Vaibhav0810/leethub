f98a1db5d1a868e4e34a7701ee77c1f73e3fb9fa
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2025-08-09 12:55:11 +0530

    Time: 152 ms (12.18%), Space: 160.7 MB (7.2%) - LeetHub

--- 2582-minimum-score-of-a-path-between-two-cities/2582-minimum-score-of-a-path-between-two-cities.cpp
+++ 2582-minimum-score-of-a-path-between-two-cities/2582-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,34 +1,41 @@
 class Solution {
 public:
+    //  can be done both using bfs and dijkstra 
+    // bfs with queue Time - O(N+M)
+    // dijkstra with pq time - O(m logn)
+
     int minScore(int n, vector<vector<int>>& roads) {
-        vector<int>dist(n,INT_MAX);
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        vector<int> dist(n, INT_MAX);
+        // priority_queue<pair<int, int>, vector<pair<int, int>>,
+        //                greater<pair<int, int>>>
+        //     pq;
+        queue<pair<int,int>>pq;
         ///////// wt,    node
-        pq.push({INT_MAX,0});
-        vector<vector<pair<int,int>>>adj(n);
-        for(auto it:roads){
-            int u=it[0];
-            int v=it[1];
-            int w=it[2];
-            adj[u-1].push_back({v-1,w});
-            adj[v-1].push_back({u-1,w});
+        pq.push({INT_MAX, 0});
+        vector<vector<pair<int, int>>> adj(n);
+        for (auto it : roads) {
+            int u = it[0];
+            int v = it[1];
+            int w = it[2];
+            adj[u - 1].push_back({v - 1, w});
+            adj[v - 1].push_back({u - 1, w});
         }
 
-        while(!pq.empty()){
-            auto prv=pq.top();
+        while (!pq.empty()) {
+            auto prv = pq.front();
             pq.pop();
-            int wt=prv.first;
-            int node=prv.second;
-            for(auto it:adj[node]){
-                int adjNode=it.first;
-                int currwt=it.second;
-                int miniwt=min(currwt,wt);
-                if(dist[adjNode]>miniwt){
-                dist[adjNode]=miniwt;
-                pq.push({miniwt,adjNode});
+            int wt = prv.first;
+            int node = prv.second;
+            for (auto it : adj[node]) {
+                int adjNode = it.first;
+                int currwt = it.second;
+                int miniwt = min(currwt, wt);
+                if (dist[adjNode] > miniwt) {
+                    dist[adjNode] = miniwt;
+                    pq.push({miniwt, adjNode});
                 }
             }
         }
-        return (dist[n-1]);
+        return dist[n - 1];
     }
 };
